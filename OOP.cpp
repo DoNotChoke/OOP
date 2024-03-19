8f1393990ecf32eecd47671aacc12d314d296db8
TuanNghia <nghiahh2004@gmail.com>
2024-03-19 21:59:12 +0700

    update oop.cpp

--- OOP.cpp
+++ OOP.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    cout<<"nghia";
 }
