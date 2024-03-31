0b7d6054fe41280fc7304f2b224f8c55dbb65661
Vu-Hoang Duong <xyzd.dvh@gmail.com>
2024-03-31 15:07:36 +0700

    xoa file OOP.cpp

--- OOP.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-    ios_base::sync_with_stdio(false);
-    cout.tie(nullptr);
-    cin.tie(nullptr);
-    cout<<"nghia\n";
-    cout << "hoang\n";
-}
