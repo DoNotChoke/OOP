49b07c49490170bddeda46241e9af467e9ab4266
nttrung612 <trung.nt225105@sis.hust.edu.vn>
2024-04-02 01:20:39 +0700

    update b4

--- bai4.cpp
+++ bai4.cpp
@@ -2,6 +2,11 @@
 #include "bai3.cpp"
 using namespace std;
 
+vector<pair<Ward, int>> generatePairs(vector<Ward> wards, int triple, int single) {
+    vector<pair<Ward, int>> pairs;
+    
+}
+
 int main () {
     cout << "ok";
 }
